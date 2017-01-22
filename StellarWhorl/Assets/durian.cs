256f0f8e2e29c4ffe23b735c49d66d34fefdb3f1
cyangoon <crystalcyst@gmail.com>
2017-01-22 13:40:59 -0800

    victory

---
+++ StellarWhorl/Assets/durian.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class durian : MonoBehaviour {
+
+    public GameObject player;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 difference = player.transform.position - transform.position;
+        difference = difference.normalized *666;
+        rb.AddForce(difference);
+    }
+
+}
